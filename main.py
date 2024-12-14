dc057e188c84f666dbebc1df27d5e8469c7efbf1
unknown <gabrieloliveiravaladares@gmail.com>
2024-12-14 15:24:30 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+nomes = ["Natascha", "Gabriel", "Lourdes", "Cristiano", "Matheus", "Manoela", "Luis"]
+
+outroNome = input("Digite outro nome: ")
+
+numero = int(input("Digite um número de 0 a 7: "))
+
+nomes.append(outroNome)
+
+print(nomes[numero])
