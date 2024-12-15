3d2d7f2173ba4aec6c929e7ef155f8795bb919a8
unknown <gabrieloliveiravaladares@gmail.com>
2024-12-15 13:57:54 -0300

    adicionado math_method

---
+++ math_method.py
@@ -0,0 +1,20 @@
+#Math
+import math
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+print(math.pow(-3,3))
+print(abs((math.pow(-3,3))))
+print(math.sqrt(9))
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
+
+print(math.pi)
